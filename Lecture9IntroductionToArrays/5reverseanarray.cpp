2744cab400a0fc09a1af3361267dd6dbb249efaa
yashyc7 <yashc9411407582@gmail.com>
2024-04-26 07:58:26 +0530

    single pointer approach using only i variable ..

--- Lecture9IntroductionToArrays/5reverseanarray.cpp
+++ Lecture9IntroductionToArrays/5reverseanarray.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main()
 {
-int arr[5]={1,2,3,4,5};
-int n=5;
+int arr[6]={1,2,3,4,5,6};
+int n=6;
 
 // int s=0,e=4;
 // while(s<e)
@@ -18,13 +18,13 @@ int n=5;
 //todo look for this approach ; 
 for(int i=0;i<n;i++)
 {
-    while(i<n-1-i)
-    {
-        swap(arr[i],arr[n-1-i]);
-    }
+   if(i<n-i-1)
+   {
+    swap(arr[i],arr[n-i-1]);
+   }
 }
 
-for(int i=0;i<5;i++)
+for(int i=0;i<n;i++)
 {
     cout<<arr[i]<<" ";
 }
